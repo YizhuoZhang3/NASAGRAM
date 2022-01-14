97ca90ca8f08a570206acc96708866d1172c8709
Zoe Zhang <zhangyizhuozoe@gmail.com>
2022-01-14 14:09:33 -0500

    render images

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,11 +2,15 @@ import React from "react";
 
 const Card = (props) => {
   const image = props.picture;
+
   return (
-    <div className="CardContainer">
-     
+    <div className="card-container">
+      <figure>
         <img src={image.url} alt={image.title} />
-    
+        <figcaption>{image.title}</figcaption>
+        <div className="date">Published on {image.date}</div>
+        <p className="desc">{image.explanation} </p>
+      </figure>
     </div>
   )
 }
