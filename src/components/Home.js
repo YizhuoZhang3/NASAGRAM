9f56693ec3326590fd16b4257936d32f25a20cea
Zoe Zhang <zhangyizhuozoe@gmail.com>
2022-01-26 20:28:52 -0500

    home page style

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,8 +3,10 @@ import { Link } from "react-router-dom";
 
 export default function Home(){
   return (
-    <div>
+    <div className="home-page">
+      <div className="homelink">
       <Link to='/nasaphoto'>See into the stars!</Link>
+      </div>
     </div>
   )
 }
